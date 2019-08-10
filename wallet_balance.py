1645eff51483adc2332e1d4c82bf9e1e91d5f1e2
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 14:20:32 +0300

    summary

--- wallet_balance.py
+++ wallet_balance.py
@@ -1,8 +1,8 @@
 import phpsap,requests as rq
 
-phpsap.apiKey='your api key'
-phpsap.username='username'
+apiKey='rcjhln5y33a9rjbv9bi8ihg1ox6tsq3fm40gnti8q290wif1sgc89f00mhc8hmwr'
+username='sir_lutta'
 
-sap_payload=dict(username=phpsap.username,apiKey=phpsap.apiKey)
+sap_payload=dict(username=username,apiKey=apiKey)
 wallet_balance_request=rq.get(phpsap.payment_balance_url,json=sap_payload, headers=phpsap.headers)
 print(wallet_balance_request.text)
