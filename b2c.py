63d907ee44feffcd988ab1a648769ac7f053fa39
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 15:13:49 +0300

    Create b2c.py

---
+++ b2c.py
@@ -0,0 +1,12 @@
+import phpsap,requests as rq
+
+apiKey='your key'
+username='username'
+
+PhoneNumber='number'
+Amount='amount'
+
+b2c_payload=dict(username=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
+
+b2c_request = rq.get(phpsap.mobileb2c_url,json=b2c_payload, headers=phpsap.headers)
+print(b2c_request.text)
