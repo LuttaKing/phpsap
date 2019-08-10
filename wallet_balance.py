909bb00814ea30bd3a0240d6994c8feb87eb71d8
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 15:24:14 +0300

    Update wallet_balance.py

--- wallet_balance.py
+++ wallet_balance.py
@@ -1,7 +1,7 @@
 import phpsap,requests as rq
 
-apiKey='rcjhln5y33a9rjbv9bi8ihg1ox6tsq3fm40gnti8q290wif1sgc89f00mhc8hmwr'
-username='sir_lutta'
+apiKey='rcjhhmwr'
+username='username'
 
 sap_payload=dict(username=username,apiKey=apiKey)
 wallet_balance_request=rq.get(phpsap.payment_balance_url,json=sap_payload, headers=phpsap.headers)
