80b60d343e83a38d80f60b3e5adb7016dddb6fc4
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 14:23:19 +0300

    Update mobile_stk_push.py

--- mobile_stk_push.py
+++ mobile_stk_push.py
@@ -1,11 +1,11 @@
 import phpsap,requests as rq
 
-phpsap.apiKey='your key'
-phpsap.username='username'
+apiKey='your key'
+username='username'
 
 PhoneNumber='put phone number'
 Amount='10' #should be string minimum 10
 
-mobile_payload=dict(username=phpsap.username,apiKey=phpsap.apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
+mobile_payload=dict(username=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
 mobile_request = rq.get(phpsap.Mobile_Push_url,json=mobile_payload, headers=phpsap.headers)
 print(mobile_request.text)
