4479bdca78dc551d2e798dbecf211bc7d95a050d
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 12:42:25 +0300

    Update phpsap.py

--- phpsap.py
+++ phpsap.py
@@ -9,7 +9,7 @@ headers={
 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0'
 }
 
-apiKey='you api key'
+apiKey='you api keys'
 username='your username'
 
 Receiver=''
