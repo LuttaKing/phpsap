b62d04d60f9bbc7c327d3079f8ce60f272d4c9b2
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 14:31:14 +0300

    Update sap_balance.py

--- sap_balance.py
+++ sap_balance.py
@@ -1,8 +1,8 @@
 import phpsap,requests as rq
 
-phpsap.apiKey='your api key'
-phpsap.username='sir_lutta'
+apiKey='your api key'
+username='sir_lutta'
 
-sap_payload=dict(username=phpsap.username,apiKey=phpsap.apiKey)
+sap_payload=dict(username=username,apiKey=apiKey)
 sap_balance_request=rq.get(phpsap.sap_balance_url,json=sap_payload, headers=phpsap.headers)
 print(sap_balance_request.text)
