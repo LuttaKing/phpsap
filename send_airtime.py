80bebd9a725db293fba71ea9303f5f5c5a2547c6
sir_lutta <38002486+LuttaKing@users.noreply.github.com>
2019-08-10 14:26:19 +0300

    Update send_airtime.py

---
+++ send_airtime.py
@@ -0,0 +1,11 @@
+import phpsap,requests as rq
+
+apiKey='your key'
+username='username'
+
+PhoneNumber='put phone number'
+Amount='10' #should be string minimum 10
+
+airtime_payload=dict(username=username,apiKey=apiKey,Receiver=PhoneNumber,Amount=Amount)
+airtime_request = rq.get(phpsap.airtime_url,json=airtime_payload, headers=phpsap.headers)
+print(airtime_request.text)
