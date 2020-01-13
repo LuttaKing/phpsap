7a61ac53378d8eadaedb4cf775b3c948162974f6
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-01-13 08:44:14 +0300

    Ghjgf

--- b2b.py
+++ b2b.py
@@ -5,7 +5,7 @@ username='usernamvjjea'
 
 paybill='paybill no'
 Amount='amount'
-acc_no='account_numbers'
+acc_no='accohjjunt_numbers'
 
 b2b_payload=dict(username=username,apiKey=apiKey,DestinationChannel=paybill,DestinationAccount=acc_no,Amount=Amount)
 b2b_request=rq.get(phpsap.mobileb2b_url,json=b2b_payload,headers=phpsap.headers)
