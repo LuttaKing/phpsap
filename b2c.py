847e690a56efa718dd6dde62f6c3d7883960fc82
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-01-28 16:54:25 +0300

    Ghhh

--- b2c.py
+++ b2c.py
@@ -4,7 +4,7 @@ apiKey='your key'
 username='usefrname'
 
 PhoneNumber='numfber'
-Amount='amouvjjnt'
+Amount='amouvjjghjnt'
 
 b2c_payload=dict(username=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
 
