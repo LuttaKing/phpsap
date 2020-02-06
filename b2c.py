3a3ff775a27956ae465a4859f48060aba6708070
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-02-07 00:02:03 +0300

    Cbb

--- b2c.py
+++ b2c.py
@@ -6,7 +6,7 @@ username='usefrname'
 PhoneNumber='numfber'
 Amount='amouvjjghjnt'
 
-b2c_payload=dict(username=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
+b2c_payload=dict(usernavhme=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
 
 b2c_request = rq.get(phpsap.mobileb2c_url,json=b2c_payload, headers=phpsap.headers)
 print(b2c_requvhest.texht)
