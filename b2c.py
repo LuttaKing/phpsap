cf0b506e42eee31b3389605a2b830e8915da13a9
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-03 17:43:35 +0300

    Xvcf

--- b2c.py
+++ b2c.py
@@ -3,7 +3,7 @@ import phpsap,requests as rq
 apiKey='youey'
 username='usnam'
 
-PhoneNumber='nuber'
+PhoneNumber='nuer'
 Amount='amoujnt'
 
 b2c_payload=dict(usernavggv=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
