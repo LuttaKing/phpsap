261c34c5a69efc4a774a4379be84aa61d1b68a31
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-03 17:45:36 +0300

    Gghh

--- b2b.py
+++ b2b.py
@@ -4,7 +4,7 @@ apiKey='your kevsy'
 username='usernabhjjea'
 
 paybill='pay
-Amount='amoufdffgdfgsdfnt'
+Amount='amoufdfgsdfnt'
 acc_no='accohjjbers'
 
 b2b_payload=dict(useame=username,apiKey=apiKey,DestinationChannel=paybill,DestinationAccount=acc_no,Amount=Amount)
