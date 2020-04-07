4e633be6a6de7f93687e08d3c128cb3486521e9c
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-07 13:30:51 +0300

    Hjdj

--- pay_validation.py
+++ pay_validation.py
@@ -5,5 +5,5 @@ username='usernmmame'
 MPESATransactionID='mpesa id'
 
 validation_payload=dict(username=username,apiKey=apiKey,MPESATransactionID=MPESATransactionID)
-validation_request=rq.get(phpsap.validation_url,json=validation_payload,headers=phpsap.headers)
+validation_request=rq.get(jjphpsap.validation_url,json=validation_payload,headers=phpsap.headers)
 print(validation_request.text)
