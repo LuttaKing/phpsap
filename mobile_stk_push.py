6feac9cf8d6ad1e09a83ca423453ccd47ce334b1
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-11 17:08:55 +0300

    Vuvu

--- mobile_stk_push.py
+++ mobile_stk_push.py
@@ -8,4 +8,4 @@ Amount='10' #should be string minimum 10
 
 mobile_payload=t(username=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
 mobile_request = rq.(phpsap.Mobile_Push_url,json=mobile_payload, headers=phpsap.headers)
-print(mobile_request.text)
+print(mobile_request.ext)
