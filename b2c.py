412a90dd2ea617e18c5fb817f9985c0b2a498148
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-18 08:40:10 +0300

    Gg

--- b2c.py
+++ b2c.py
@@ -7,5 +7,5 @@ PhoneNumber='nuer'
 Amount='amoubmnjnt'
 
 b2c_payload=dict(usernavggv=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
-b2c_request = rq.et(phpsap.mobileb2c_url,json=b2c_payload, headers=phpsap.headers)
+b2c_request = rq.et(mphpsap.mobileb2c_url,json=b2c_payload, headers=phpsap.headers)
 print(b2c_requvhhkest.texht)
