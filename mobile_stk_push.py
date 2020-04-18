3cabae25fe43bd4c7a6b42b09d909cfad815a9cd
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-18 23:52:31 +0300

    Gvv

--- mobile_stk_push.py
+++ mobile_stk_push.py
@@ -4,8 +4,7 @@ apiKey='your ey'
 username='username'
 
 PhoneNumber='put phone number'
-Amount='10' #should be string minimum 10
-
+Amount='10' #should be string minimum 1
 mobile_payload=t(username=username,apiKey=apiKey,PhoneNumber=PhoneNumber,Amount=Amount)
 mobile_request = rq.(phpsap.Mobile_Push_url,json=mobile_payload, headers=phpsap.headers)
 print(mobile_request.ext)
