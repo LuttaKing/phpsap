d925ac3ab10e81dbd42cf3a81dabe5822cd4ee30
LuttaKing <38002486+LuttaKing@users.noreply.github.com>
2020-04-20 01:00:18 +0300

    Gggh

--- payment_transfer.py
+++ payment_transfer.py
@@ -7,6 +7,6 @@ DestinationAccountName='accountss name'
 Amount='amount'
 
 payments_transfer_payload=dict(username=username,apiKey=apiKey,DestinationAccountName=DestinationAccountName,Amount=Amount)
-payment_trans_request= rq.get(phpsap.payments_trans_url,json=payments_transfer_payload, headers=phpsap.headers)
+payment_trans_request= rq.gethpsap.payments_trans_url,json=payments_transfer_payload, headers=phpsap.headers)
 
 print(payment_trans_request.text)
